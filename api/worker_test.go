96723032ac13da0c7840bd5e912be8c95870033e
Stephen Carragher <car07009312@hotmail.co.uk>
2018-05-26 23:37:42 +0100

    .

---
+++ api/worker_test.go
@@ -0,0 +1,19 @@
+package fgrep
+
+import "testing"
+
+func TestDoWork(t *testing.T) {
+	worker := Worker{ID: 1}
+
+	worker.WorkFunc = func() {
+		if worker.Status != StatusWorking {
+			t.Errorf("Expected %s while working, got %s", StatusWorking, worker.Status)
+		}
+	}
+
+	worker.DoWork()
+
+	if worker.Status != StatusIdle {
+		t.Errorf("Expected status %s when finished, got %s", StatusIdle, worker.Status)
+	}
+}
